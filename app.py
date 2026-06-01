0ec379c31257e8c6d04b675fe61b9bfa681fdcbb
Jordan Adeoko <jordanandroko@gmail.com>
2026-06-01 08:03:35 +0000

    feat: render HTML interface

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({
-        "status": "online",
-        "message": "Prototype Flask - Pipeline CI/CD fonctionnel !",
-        "version": "1.0.0"
-    })
+    return render_template('index.html')
 
 @app.route('/health')
 def health():
-    return jsonify({"status": "healthy"})
+    return jsonify({"status": "healthy", "version": "1.0.0"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
